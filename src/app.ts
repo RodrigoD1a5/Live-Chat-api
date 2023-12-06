188f8103a561b5fd096eedb4653ca4cc05c3e783
RodrigoD1a5 <rodrigobastos.diass@gmail.com>
2023-12-06 17:34:24 -0300

    feat: add rooms endpoint to list all rooms

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import fastify from "fastify";
 import fastifySocketIO from "fastify-socket.io";
 
 import { envs } from "./lib/envs";
+import { roomsRoute } from "./routes/rooms-route";
 import { socketIo } from "./routes/socket-io";
 import { usersRoute } from "./routes/users-route";
 
@@ -10,12 +11,12 @@ const app = fastify();
 app.register(fastifySocketIO, {
     cors: { origin: "*" },
 });
-
 app.register(fastifyJwt, {
     secret: envs.JWT_SECRET,
 });
 
 app.register(usersRoute);
+app.register(roomsRoute, { prefix: "/rooms" });
 app.register(socketIo);
 
 export { app };
